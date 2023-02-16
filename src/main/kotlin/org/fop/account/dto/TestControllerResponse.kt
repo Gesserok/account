8e1a3393830f828ad19b6bf6e824dc0e036c80cb
Gesserok <if2102@outlook.com>
2023-02-16 10:35:25 +0200

    amount

--- src/main/kotlin/org/fop/account/dto/TestControllerResponse.kt
+++ src/main/kotlin/org/fop/account/dto/TestControllerResponse.kt
@@ -2,5 +2,6 @@ package org.fop.account.dto
 
 data class TestControllerResponse(
     val msg: String = "Hello from Account Service",
-    val ip: String
+    val ip: String,
+    val amount: String = "You have got a lot of money"
 )
